b1bc943d27bf411ffe07f83b49b6ebd960175548
vm4321minus <staskulesh.brest@gmail.com>
2022-02-28 23:18:07 +0300

    feat: add function burger menu

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
 import './main.scss';
 import html from './index.html';
 
+const btnBurger = document.querySelector('.menu__icon'),
+    navContent = document.querySelector('.header__nav'),
+    headerLinks = document.querySelectorAll('.header__link');
+
+function clickBurger() {
+    navContent.classList.toggle('active');
+    btnBurger.classList.toggle('btn__active');
+    changeRoot();
+  
+}
+
+function clickLink() {
+    headerLinks.forEach(el => {
+        el.addEventListener('click', () => {
+            if(navContent.classList.contains('active')) {
+                navContent.classList.remove('active');
+                btnBurger.classList.remove('btn__active');
+                changeRoot();
+            }
+        })
+    })
+}
+
+function changeRoot() {
+    if(btnBurger.classList.contains('btn__active')) {
+        document.documentElement.style.setProperty('--pos', 'fixed');
+    }
+    else {
+        document.documentElement.style.setProperty('--pos', 'absolute');
+    }
+}
+
+btnBurger.addEventListener('click', clickBurger);
+clickLink();
